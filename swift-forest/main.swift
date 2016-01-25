2d52c45d0b0aee780cab7c1ac1d7262d67dad611
Will Cannings <me@willcannings.com>
2016-01-25 18:04:34 +1100

    Initial Commit

---
+++ swift-forest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-forest
+//
+//  Created by Will Cannings on 25/01/2016.
+//  Copyright © 2016 Tzukuri. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
